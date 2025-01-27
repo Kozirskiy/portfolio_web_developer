b8c899e81f787e37de895e6f47690ccdd6315cd9
Atlas <atlasmail@atlas.com>
2025-01-27 21:46:10 +0100

    second commit from web

--- src/compponents/styles/GlobalStyles.tsx
+++ src/compponents/styles/GlobalStyles.tsx
@@ -18,6 +18,7 @@ export const GlobalStyles = createGlobalStyle`
         font-family: "Poppins", serif;
         font-weight: 400;
         font-style: normal;
+        
     }
 
     h1, h2, h3, h4, h5, h6, p {
